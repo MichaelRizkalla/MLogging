93abf2da2d970e6de9313b669fc131038611a72f
Michael Rizkalla <Michael.Anis1995@gmail.com>
2021-02-14 20:42:27 +0000

    Fix non-standard template names

--- MLogging/Src/Public/Defines/MLoggingTraits.h
+++ MLogging/Src/Public/Defines/MLoggingTraits.h
@@ -2,48 +2,48 @@
 
 namespace MLog {
 
-	template <class _Ty, class TFirst, class... Args>
+	template <class Ty, class TFirst, class... Args>
 	struct is_in_stream_friendly {
 	public:
-		constexpr static bool value = is_in_stream_friendly<_Ty, TFirst>::value &
-										is_in_stream_friendly<_Ty, Args...>::value;
+		constexpr static bool value = is_in_stream_friendly<Ty, TFirst>::value &
+										is_in_stream_friendly<Ty, Args...>::value;
 	};
 
 	/// <summary>
 	/// Type trait to test whether a class can accept input stream
 	/// </summary>
-	template <class _Ty, class _Tx>
-	struct is_in_stream_friendly <_Ty, _Tx> {
+	template <class Ty, class Tx>
+	struct is_in_stream_friendly <Ty, Tx> {
 	private:
-		template <class _Target, class _Test>
-		constexpr static auto is_friendly(int) -> decltype(std::declval<_Target&>() >> std::declval<_Test>(), std::true_type());
+		template <class Target, class Test>
+		constexpr static auto is_friendly(int) -> decltype(std::declval<Target&>() >> std::declval<Test>(), std::true_type());
 
 		template <class, class>
 		constexpr static auto is_friendly(...)->std::false_type;
 	public:
-		constexpr static bool value = decltype(is_friendly<_Ty, _Tx>(0))::value;
+		constexpr static bool value = decltype(is_friendly<Ty, Tx>(0))::value;
 	};
 
-	template <class _Ty, class TFirst, class... Args>
+	template <class Ty, class TFirst, class... Args>
 	struct is_out_stream_friendly {
 	public:
-		static constexpr bool value = is_out_stream_friendly<_Ty, TFirst>::value &
-										is_out_stream_friendly<_Ty, Args...>::value;
+		static constexpr bool value = is_out_stream_friendly<Ty, TFirst>::value &
+										is_out_stream_friendly<Ty, Args...>::value;
 	};
 
 	/// <summary>
 	/// Type trait to test whether a class can be streamed
 	/// </summary>
-	template <class _Ty, class _Tx>
-	struct is_out_stream_friendly<_Ty, _Tx> {
+	template <class Ty, class Tx>
+	struct is_out_stream_friendly<Ty, Tx> {
 	private:
-		template <class _Target, class _Test>
-		static auto is_friendly(int) -> decltype(std::declval<_Target&>() << std::declval<_Test>(), std::true_type());
+		template <class Target, class Test>
+		static auto is_friendly(int) -> decltype(std::declval<Target&>() << std::declval<Test>(), std::true_type());
 
 		template <class, class>
 		static auto is_friendly(...)->std::false_type;
 	public:
-		static constexpr bool value = decltype(is_friendly<_Ty, _Tx>(0))::value;
+		static constexpr bool value = decltype(is_friendly<Ty, Tx>(0))::value;
 	};
 
 }
